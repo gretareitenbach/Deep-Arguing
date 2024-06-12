e09e30ec224b150d99786894dd0020d0e516e109
TheAdamG <adam.gould39@gmail.com>
2024-06-12 18:17:19 +0100

    Created torch implementation of AA-CBR

--- aacbr_torch.py
+++ aacbr_torch.py
@@ -1,14 +1,16 @@
-import numpy as np
+import torch
 import networkx as nx
 import matplotlib.pyplot as plt
+import numpy as np
 
 
-class AACBR:
+class AACBRTorch(torch.nn.Module):
 
     def __init__(self, X_train, y_train, comparison_func, default_case, default_outcomes,
                  use_symmetric_attacks=True, build_parallel=False, casebase_indices=None) -> None:
-        
-        assert(len(X_train) == len(y_train))
+
+        super(AACBRTorch, self).__init__()
+        assert (len(X_train) == len(y_train))
         self.comparison_func = comparison_func
         self.X_train = X_train
         self.y_train = y_train
@@ -17,46 +19,45 @@ class AACBR:
         self.use_symmetric_attacks = use_symmetric_attacks
         self.casebase_indices = casebase_indices
 
-
         self.add_default_cases(default_case, default_outcomes)
         if build_parallel:
             self.build_af_parallel()
         else:
             self.build_af()
-        
-        
 
     def add_default_cases(self, default_case, default_outcomes):
 
         pre = len(self.X_train)
-        self.X_train = np.append(
-            self.X_train, [default_case]*len(default_outcomes), axis=0)
-        self.y_train = np.append(self.y_train, default_outcomes)
+        self.X_train = torch.cat(
+            (self.X_train, default_case.repeat(len(default_outcomes), 1)), dim=0)
+        self.y_train = torch.cat((self.y_train, default_outcomes), dim=0)
+
         post = len(self.X_train)
         self.default_indexes = list(range(pre, post))
         # self.outcome_map = {outcome: self.default_indexes[i] for i, outcome in enumerate(self.default_outcomes)}
-    
+
     def get_indices(self):
         if self.casebase_indices is None:
             return list(range(len(self.X_train)))
         else:
-            return np.concatenate((self.casebase_indices, np.array(self.default_indexes)))
+            return torch.cat((self.casebase_indices, self.default_indexes))
 
     def build_af(self):
         # Builds an AF with a matrix representation with a size of len(X_train)
         # If casebase_indices is  specified, only cases at these indices will be used to build the AF
-        # which can result in a matrix that is larger than necessary but has a fixed size 
+        # which can result in a matrix that is larger than necessary but has a fixed size
         # and nodes in a fixed location if the casebase changes
         # TODO: Use more efficient implementation that sorts topologically as in AA-CBR Library
 
-        A = np.zeros((len(self.X_train), len(self.X_train)), dtype=np.float32)
+        A = torch.zeros((len(self.X_train), len(self.X_train)),
+                        dtype=torch.float32)
 
         for i in self.get_indices():
-            attacker = self.X_train[i] 
+            attacker = self.X_train[i]
             if i in self.default_indexes:
                 continue
             for j in self.get_indices():
-                target = self.X_train[j] 
+                target = self.X_train[j]
                 if self.y_train[i] == self.y_train[j]:
                     continue
 
@@ -67,57 +68,45 @@ class AACBR:
 
         self.A = A
 
-    def cartesian_product_simple_transpose(self, arrays):
-        # https://stackoverflow.com/questions/11144513/cartesian-product-of-x-and-y-array-points-into-single-array-of-2d-points
-        la = len(arrays)
-        dtype = np.result_type(*arrays)
-        arr = np.empty([la] + [len(a) for a in arrays], dtype=dtype)
-        for i, a in enumerate(np.ix_(*arrays)):
-            arr[i, ...] = a
-        return arr.reshape(la, -1).T
-
     def build_af_parallel(self):
 
         # TODO: Could find a way to make use of Topological ordering to speed this up further
         train_size = len(self.X_train)
-        indexes = self.cartesian_product_simple_transpose(
-            [np.arange(train_size),
-             np.arange(train_size),
-             np.arange(train_size)
-             ]
+        all_indices = torch.arange(train_size)
+        indexes = torch.cartesian_prod(
+            [all_indices, all_indices,  all_indices]
         )
 
-        # [0, 0, 0]
-        # [0, 0, 1]
-        # [0, 0, 2]
-        # [0, 1, 0]
-        # [0, 1, 1]
-        # [0, 1, 2]
-        ...
-        # [2, 2, 2]
+        attackers, attackers_labels = self.X_train[indexes[:, 0]
+                                                   ], self.y_train[indexes[:, 0]]
+        targets, targets_labels = self.X_train[indexes[:, 1]
+                                               ], self.y_train[indexes[:, 1]]
+        blockers, blockers_labels = self.X_train[indexes[:, 2]
+                                                 ], self.y_train[indexes[:, 2]]
 
-        attackers, attackers_labels = self.X_train[indexes[:, 0]], self.y_train[indexes[:, 0]]
-        targets, targets_labels     = self.X_train[indexes[:, 1]], self.y_train[indexes[:, 1]]
-        blockers, blockers_labels   = self.X_train[indexes[:, 2]], self.y_train[indexes[:, 2]]
-
-        attackers_mask = np.isin(indexes[:, 0], self.get_indices())
-        targets_mask = np.isin(indexes[:, 1], self.get_indices())
-        blockers_mask = np.logical_not(np.isin(indexes[:, 2], self.get_indices()))
+        attackers_mask = torch.isin(indexes[:, 0], self.get_indices())
+        targets_mask = torch.isin(indexes[:, 1], self.get_indices())
+        blockers_mask = torch.logical_not(
+            torch.isin(indexes[:, 2], self.get_indices()))
         # index_mask = True
 
+        potential_attacks = torch.logical_and(
+            attackers_labels != targets_labels, self.comparison_func(attackers, targets))
+        is_blocked = torch.logical_and(blockers_labels == attackers_labels, torch.logical_and(
+            self.comparison_func(attackers, blockers), self.comparison_func(blockers, targets)))
+        symmetric_attacks = torch.logical_and(self.use_symmetric_attacks, torch.logical_and(
+            attackers_labels != targets_labels, torch.all(attackers == targets, axis=1)))
+        attacks = torch.logical_or(torch.logical_and(
+            potential_attacks, torch.logical_not(is_blocked)), symmetric_attacks)
 
-        potential_attacks = np.logical_and(attackers_labels != targets_labels, self.comparison_func(attackers, targets))
-        is_blocked = np.logical_and(blockers_labels == attackers_labels, np.logical_and(self.comparison_func(attackers, blockers), self.comparison_func(blockers, targets)))
-        symmetric_attacks = np.logical_and(self.use_symmetric_attacks, np.logical_and(attackers_labels != targets_labels, np.all(attackers == targets, axis=1)))
-        attacks = np.logical_or(np.logical_and(potential_attacks, np.logical_not(is_blocked)), symmetric_attacks)
-        
         attacks[blockers_mask] = True
-        attacks = np.logical_and(attacks, np.logical_and(attackers_mask, targets_mask))
+        attacks = torch.logical_and(
+            attacks, torch.logical_and(attackers_mask, targets_mask))
 
         attacks = attacks.reshape((train_size, train_size, train_size))
-        attacks = np.all(attacks, axis=-1)
+        attacks = torch.all(attacks, axis=-1)
         attacks[self.default_indexes, :] = False
-        self.A = np.where(attacks, -1, 0)
+        self.A = torch.where(attacks, -1, 0)
 
     def is_concise(self, attacker, target, attacker_outcome):
         return not any([
@@ -125,7 +114,8 @@ class AACBR:
             self.comparison_func(attacker, self.X_train[k])[0] and self.comparison_func(self.X_train[k], target)[0] for k in self.get_indices()])
 
     def show_graph_with_labels(self):
-        rows, cols = np.where(self.A != 0)
+        A = self.A.detach().cpu().numpy()
+        rows, cols = np.where(A != 0)
         edges = zip(rows.tolist(), cols.tolist())
 
         gr = nx.DiGraph()
@@ -165,9 +155,8 @@ class AACBR:
 
     def show_matrix(self):
         plt.figure(figsize=(10, 10))
-
-        B = self.A.copy()
-        plt.imshow(B, cmap='viridis', interpolation='nearest')
+        A = self.A.detach().cpu().numpy()
+        plt.imshow(A, cmap='viridis', interpolation='nearest')
         plt.colorbar(label='Value')
         plt.xlabel('Nodes')
         plt.ylabel('Nodes')
@@ -176,16 +165,16 @@ class AACBR:
 
     def get_new_case_attacks_mask(self, new_cases):
         if new_cases.ndim == 1:
-            new_cases = np.expand_dims(new_cases, axis=0)
-        
-        result = np.logical_not(self.comparison_func(
-            new_cases[:, np.newaxis, :], self.X_train))
+            new_cases = new_cases.unsqueeze(0)
+
+        result = torch.logical_not(self.comparison_func(
+            new_cases.unsqueeze(1), self.X_train))
 
         # Default should not be attacked by new case
         result[:, self.default_indexes] = False
 
         # Cases that are not in the casebase indices are not attacked
-        mask = np.ones(len(self.X_train), dtype=bool)
+        mask = torch.ones(len(self.X_train), dtype=bool)
         mask[self.get_indices()] = False
         result[:, mask] = False
 
@@ -194,14 +183,14 @@ class AACBR:
     def compute_grounded(self, new_cases_attacks):
 
         if new_cases_attacks.ndim == 1:
-            new_cases_attacks = np.expand_dims(new_cases_attacks, axis=0)
+            new_cases_attacks = new_cases_attacks.unsqueeze(0)
 
-        A = self.A.copy()
+        A = self.A.clone()
 
         batch_size = new_cases_attacks.shape[0]
 
         # Batch A - to support multiple new_cases at once
-        A = np.tile(A[np.newaxis, :, :], (batch_size, 1, 1))
+        A = torch.tile(A.unsqueeze(0), (batch_size, 1, 1))
 
         A[new_cases_attacks, :] = 0
 
@@ -209,33 +198,33 @@ class AACBR:
         # For each node, x, that they attack, set all attacks originating from x to 0
         # Repeat until no more changes
         while True:
-            unattacked = np.logical_and(np.all(A == 0, axis=1), np.logical_not(
+            unattacked = torch.logical_and(torch.all(A == 0, axis=1), torch.logical_not(
                 new_cases_attacks))  # Shape: B x n
 
-            mask = unattacked[:, :, np.newaxis]
+            mask = unattacked.unsqueeze(2)
 
             # Get the rows of the adjacency matrix that are unattacekd and zero the attacked rows
-            only_unattacked = np.where(mask, A, 0)
+            only_unattacked = torch.where(mask, A, 0)
 
             # Get the mask of the rows that are attacked by unattacked rows
-            attacked = np.any(only_unattacked != 0, axis=1)  # Shape: B x n
+            attacked = torch.any(only_unattacked != 0, axis=1)  # Shape: B x n
 
-            if np.all(A[attacked] == 0):
+            if torch.all(A[attacked] == 0):
                 break
 
             A[attacked, :] = 0
 
-        final_unattacked = np.logical_and(
-            np.all(A[:,] == 0, axis=1), np.logical_not(new_cases_attacks))
+        final_unattacked = torch.logical_and(
+            torch.all(A[:,] == 0, axis=1), torch.logical_not(new_cases_attacks))
 
         return final_unattacked
 
     def forward(self, new_cases):
         new_cases_attacks = self.get_new_case_attacks_mask(new_cases)
         grounded = self.compute_grounded(new_cases_attacks)
-        predicted = np.where(
+        predicted = torch.where(
             grounded[:, self.default_indexes], 1, 0)
         return predicted
 
-    def __call__(self, new_cases):
-        return self.forward(new_cases)
+    # def __call__(self, new_cases):
+    #     return self.forward(new_cases)
